2fcfdbb7aa17ca2769c2bc6acebe76eaa84d2ca2
Bence László <bencelaszlo@protonmail.com>
2023-05-12 17:45:26 +0200

    install and configure eslint

---
+++ pages/test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Head from "next/head";
+import Layout from "../components/layout";
+import Container from "../components/container";
+
+const LandingPage = () => {
+	return (    <>
+		<Layout>
+			<Head>
+				<title>{"Bence László - bencelaszlo.hu"}</title>
+			</Head>
+			<Container >
+				<div className="bg-slate-950 text-white">
+					<h1>Bence László</h1>
+
+				</div>
+				<p>bencelaszlo</p>
+				<h2>Software Engineer</h2>  
+			</Container>
+		</Layout>
+	</>);
+};
+
+export default LandingPage;
